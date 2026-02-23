4eb0c394b31dac65aa644841fc8bb56b1c76ed40
melisbm <miquelangel.melis@gmail.com>
2026-02-23 14:31:17 +0100

    list decoration with each index

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const tasksList = document.getElementById("tasks-list");
 const noTasksText = document.querySelector("#tasks-list-container p");
 
 let totalTasksOnList = 0;
+let index = 1;
 
 function addTask(taskName) {
 
     const task = document.createElement("li");
 
     const taskLabel = document.createElement("p");
+    const indexLabel = document.createElement("p");
+    indexLabel.textContent = index++;
     taskLabel.textContent = taskName;
 
+    task.appendChild(indexLabel);
     task.appendChild(taskLabel);
     tasksList.appendChild(task);
 
@@ -27,5 +31,6 @@ document.querySelector('form').addEventListener("submit", function (event) {
     event.preventDefault();
 
     const taskName = taskInput.value;
+    const taskDescription = 
     addTask(taskName);
 });
