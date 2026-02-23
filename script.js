1d9f857f7ce339d3b860271453da052682195106
melisbm <miquelangel.melis@gmail.com>
2026-02-23 14:53:47 +0100

    description on task

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const taskInput = document.getElementById('task-name-input');
+const taskDescriptionInput = document.getElementById("task-description-input");
 const tasksList = document.getElementById("tasks-list");
 const noTasksText = document.querySelector("#tasks-list-container p");
 
 let totalTasksOnList = 0;
 let index = 1;
 
-function addTask(taskName) {
+function addTask(taskName, taskDescription) {
 
     const task = document.createElement("li");
 
@@ -14,8 +15,18 @@ function addTask(taskName) {
     indexLabel.textContent = index++;
     taskLabel.textContent = taskName;
 
-    task.appendChild(indexLabel);
-    task.appendChild(taskLabel);
+    const taskDescriptionLabel = document.createElement("p");
+    const itemFront = document.createElement("div");
+    const divider = document.createElement("vr");
+    taskDescriptionLabel.textContent = taskDescription;
+
+    itemFront.appendChild(indexLabel);
+    itemFront.appendChild(taskLabel);
+    itemFront.appendChild(divider);
+
+    task.appendChild(itemFront);
+    task.appendChild(taskDescriptionLabel);
+
     tasksList.appendChild(task);
 
     totalTasksOnList++;
@@ -31,6 +42,6 @@ document.querySelector('form').addEventListener("submit", function (event) {
     event.preventDefault();
 
     const taskName = taskInput.value;
-    const taskDescription = 
-    addTask(taskName);
+    const taskDescription = taskDescriptionInput.value;
+    addTask(taskName, taskDescription);
 });
